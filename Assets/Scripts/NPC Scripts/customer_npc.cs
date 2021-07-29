3f7e85ff76c6138ed9f6087b943a65fb99ad41c3
philkwek <philkwek03@gmail.com>
2021-07-29 14:24:49 +0800

    Added NPC move script
    NPC can now randomly seat itself at any of the 6 different booths.

--- Assets/Scripts/NPC Scripts/customer_npc.cs
+++ Assets/Scripts/NPC Scripts/customer_npc.cs
@@ -7,14 +7,19 @@ using UnityEngine.AI;
 public class customer_npc : MonoBehaviour
 {
     NavMeshAgent agent; //reference to agent
-    public Transform target; //target position for NPC to travel to
-    public Transform turn_target; // target direction to turn to
+    //public Transform target; //target position for NPC to travel to
+    //public Transform turn_target; // target direction to turn to
+    public GameObject[] AI_targets; // Array of all target positions for AI to travel to
+    public GameObject[] AI_targets_face;// Array of all target position for AI to face once seat is reached
+
+    private int target_index;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>(); // gets gameobject's navmesh agent for use
-        MoveToPoint(target.position); //moves npc to target location. 
+        //MoveToPoint(target.position);
+        RandomizeTarget();
     }
 
     private void Update()
@@ -26,6 +31,13 @@ public class customer_npc : MonoBehaviour
         }
     }
 
+    public void RandomizeTarget() //this function picks a random target zone for NPC to go and seat inside the restaurant
+    {
+        target_index = Random.Range(0, 5); //produces random number for picking out which target to choose from
+        Debug.Log(target_index);
+        MoveToPoint(AI_targets[target_index].transform.position);
+    }
+
     public void MoveToPoint(Vector3 point) //this function is for moving the playerobject
     {
         agent.SetDestination(point); 
@@ -33,7 +45,7 @@ public class customer_npc : MonoBehaviour
 
     void FaceTarget() // function for turning gameObject
     {
-        Vector3 direction = (turn_target.position - transform.position).normalized;
+        Vector3 direction = (AI_targets_face[target_index].transform.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0f, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
