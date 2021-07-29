9c3c906583a13050d53c735b6d4a8e8cb75a8663
philkwek <philkwek03@gmail.com>
2021-07-29 16:11:34 +0800

    Added random NPC model choosing

--- Assets/Scripts/NPC Scripts/customer_npc.cs
+++ Assets/Scripts/NPC Scripts/customer_npc.cs
@@ -11,12 +11,15 @@ public class customer_npc : MonoBehaviour
     //public Transform turn_target; // target direction to turn to
     public GameObject[] AI_targets; // Array of all target positions for AI to travel to
     public GameObject[] AI_targets_face;// Array of all target position for AI to face once seat is reached
+    public GameObject[] NPC_Model; //Array of the 2 NPC models for use
 
     private int target_index;
+    private int npc_model;
 
     // Start is called before the first frame update
     void Start()
     {
+        RandomizeNPC();
         agent = GetComponent<NavMeshAgent>(); // gets gameobject's navmesh agent for use
         //MoveToPoint(target.position);
         RandomizeTarget();
@@ -31,9 +34,15 @@ public class customer_npc : MonoBehaviour
         }
     }
 
+    public void RandomizeNPC() //this function randomely chooses between the 2 NPC Models for use
+    {
+        npc_model = Random.Range(0, 2);
+        NPC_Model[npc_model].SetActive(true);
+    }
+
     public void RandomizeTarget() //this function picks a random target zone for NPC to go and seat inside the restaurant
     {
-        target_index = Random.Range(0, 5); //produces random number for picking out which target to choose from
+        target_index = Random.Range(0, 6); //produces random number for picking out which target to choose from
         Debug.Log(target_index);
         MoveToPoint(AI_targets[target_index].transform.position);
     }
