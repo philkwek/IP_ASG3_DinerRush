90f946a1f329398252cfdb17342bff9997e4d3c6
wyankai <73118690+wyankai@users.noreply.github.com>
2021-08-12 07:14:11 +0800

    Add Post Processing and Door Animation Code

--- Assets/Scripts/Object Scripts/doorScript.cs
+++ Assets/Scripts/Object Scripts/doorScript.cs
@@ -5,9 +5,22 @@ using UnityEngine;
 public class doorScript : MonoBehaviour
 {
     public AudioSource sound;
+    public Animator doorAnimator;
+    private bool doorOpen;
+
+    private void Update()
+    {
+        doorAnimator.SetBool("In Range", doorOpen);
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         sound.Play();
+        doorOpen = true;
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        doorOpen = false;
     }
 }
